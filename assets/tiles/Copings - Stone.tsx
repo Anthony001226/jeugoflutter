0857ccad94acd5ac07dab2afc27fe2b4a7ecff87
Overland865 <113620174+Overland865@users.noreply.github.com>
2025-11-18 21:52:15 -0600

    change

---
+++ assets/tiles/Copings - Stone.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Copings - Stone " tilewidth="64" tileheight="64" tilecount="36" columns="6">
+ <image source="D:/iso_tiled/Copings/Copings - Stone 1 64x64.png" trans="ffffff" width="384" height="384"/>
+</tileset>
---
+++ assets/tiles/tiles_box.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tiles_box" tilewidth="64" tileheight="64" tilecount="24" columns="6">
+ <image source="D:/iso_tiled/Boxes/Crates - Wood 64x64.png" trans="ffffff" width="384" height="256"/>
+</tileset>
