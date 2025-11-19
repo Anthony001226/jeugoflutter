02f4560c6fa04c396a276636d614a0f95ce08f41
Overland865 <113620174+Overland865@users.noreply.github.com>
2025-11-18 21:11:31 -0600

    download

---
+++ assets/tiles/boxes_tile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="boxes_tile" tilewidth="64" tileheight="64" tilecount="24" columns="6">
+ <image source="D:/iso_tiled/Boxes/Crates - Metal 64x64.png" trans="ff00ff" width="384" height="256"/>
+</tileset>
