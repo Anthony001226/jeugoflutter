1965b2fc2a3e0e215d50e18608dc5185107d38e0
Overland865 <113620174+Overland865@users.noreply.github.com>
2025-11-20 13:38:58 -0600

    chsnge

---
+++ assets/tiles/boxes_wod.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="boxes_wod" tilewidth="256" tileheight="256" tilecount="1014" columns="39">
+ <image source="D:/iso_tiled/Boxes/boxes_wod.png" trans="ffffff" width="1248" height="832"/>
+</tileset>
---
+++ assets/tiles/boxes_wood.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="boxes_wood" tilewidth="32" tileheight="32" tilecount="1014" columns="39">
+ <image source="D:/iso_tiled/Boxes/boxes_wood.png" trans="ffffff" width="1248" height="832"/>
+</tileset>
