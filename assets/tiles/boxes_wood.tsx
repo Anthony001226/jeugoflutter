d45a098e37a0449513ca15cdbdc081320502d1f9
Overland865 <113620174+Overland865@users.noreply.github.com>
2025-12-02 11:51:14 -0600

    change

--- assets/tiles/boxes_wood.tsx
+++
@@ -1,4 +0,0 @@
-<? xml version = "1.0" encoding = "UTF-8" ?>
-    <tileset version="1.10" tiledversion="1.11.2" name="boxes_wood" tilewidth="32" tileheight="32" tilecount="1014" columns="39">
-        <image source="boxes_wood.png" trans="ffffff" width="1248" height="832" />
-    </tileset>
